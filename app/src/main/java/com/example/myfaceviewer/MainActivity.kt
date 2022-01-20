4ffafb37ea4861aa581d1f99ad0fcf241a49f4ef
guandi <guandi@archermind.com>
2022-01-20 10:30:47 +0800

    get View height and width

--- app/src/main/java/com/example/myfaceviewer/MainActivity.kt
+++ app/src/main/java/com/example/myfaceviewer/MainActivity.kt
@@ -5,27 +5,69 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.view.ViewTreeObserver.OnGlobalLayoutListener
+
+import android.view.ViewTreeObserver
+
 
 class MainActivity : AppCompatActivity() {
-    companion object{
+    companion object {
         private const val TAG = "MainActivity"
     }
-    private lateinit var button:Button
-    private lateinit var textView:TextView
+
+    private lateinit var button: Button
+    private lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button = findViewById<Button>(R.id.button)
-        textView = findViewById<TextView>(R.id.text)
+        button = findViewById(R.id.button)
+        textView = findViewById(R.id.text)
+
+        Log.d(
+            TAG,
+            "1 onWindowFocusChanged: buttonWidth:${button.width}, textViewWidth:${textView.width}"
+        )
+
+        button.post {
+            Log.d(
+                TAG,
+                "5 onWindowFocusChanged: buttonWidth:${button.width}, textViewWidth:${textView.width}"
+            )
+        }
+
 
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
-
-        if(hasFocus){
-            Log.d(TAG, "onWindowFocusChanged: buttonWidth:${button.width}, textViewWidth:${textView.width}")
+        Log.d(TAG, "onWindowFocusChanged: hasFocus:$hasFocus")
+        if (hasFocus) {
+            Log.d(
+                TAG,
+                "onWindowFocusChanged: buttonWidth:${button.width}, textViewWidth:${textView.width}"
+            )
+            Log.d(
+                TAG,
+                "onWindowFocusChanged: buttonMeasuredWidth:${button.measuredWidth}, textViewMeasuredWidth:${textView.measuredWidth}"
+            )
         }
     }
+
+
+    override fun onStart() {
+        super.onStart()
+        button.viewTreeObserver.addOnGlobalLayoutListener(object : OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                button.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                Log.d(
+                    TAG,
+                    "viewTreeObserver onStart: buttonWidth:${button.width}, textViewWidth:${textView.width}"
+                )
+            }
+        })
+
+    }
 }
+
+
