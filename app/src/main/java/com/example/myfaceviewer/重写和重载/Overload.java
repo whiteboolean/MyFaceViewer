9c22d0f6aa06be66821b24523423f9d1ebb99a6b
whiteboolean@gmail.com <whiteboolean@gmail.com>
2022-01-22 13:34:20 +0800

    1

---
+++ app/src/main/java/com/example/myfaceviewer/重写和重载/Overload.java
@@ -0,0 +1,16 @@
+package com.example.myfaceviewer.重写和重载;
+
+/**
+ * 重载
+ */
+public class Overload {
+
+    public void test1(){
+    }
+
+    public int test1(int a ){
+        return 0 ;
+    }
+
+
+}
